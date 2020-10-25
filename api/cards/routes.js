1aaa112733f1e3a46393f0f9640ec2fe3f7083c2
Pablo <pabloacquafresca@gmail.com>
2020-10-25 17:53:09 -0300

    intento de agregar ruta /cards

---
+++ api/cards/routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+//Delete cards
+router.delete('/:userid', async (req, res) => {
+
+  const { xappid, token, pin } = req.headers;
+  const { card_id } = req.body;
+
+  console.log('xappid: ' + xappid + ' y token: ' + token + ' y pin: ' + pin);
+  console.log('ID card: ' + card_id);
+
+  var rta = {
+    msg: "Borrado de tarjeta exitoso"
+  }
+
+  if (card_id == 13) {
+
+    res.status(401)
+    res.send("Error al borrar la tarjeta")
+
+  } else {
+
+    res.send(JSON.stringify(rta));
+
+  }
+
+});
--- api/index.js
+++ api/index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 const user = require('./user/routes');
+const cards = require('./cards/routes');
 
 router.use('/user', user);
+router.use('/cards', cards);
 
 /* GET home page. */
 router.get('/', (req, res) => {
