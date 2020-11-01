b577266e2a42854790e59ad2fcc869a9787e96f2
Pablo <pabloacquafresca@gmail.com>
2020-11-01 10:47:19 -0300

    agregado ruta post /provider/bind

--- api/provider/routes.js
+++ api/provider/routes.js
@@ -27,5 +27,31 @@ router.put('/unbind', async (req, res) => {
   
   });
 
+  //Associate service to cards
+router.post('/bind', async (req, res) => {
+
+    const { xappid, token, pin } = req.headers;
+    const { provider_id, card_id } = req.body;
+  
+    console.log('xappid: ' + xappid + ' y token: ' + token + ' y pin: ' + pin);
+    console.log('Provider id: ' + provider_id + ' Card ID: ' + card_id );
+  
+    var rta = {
+      msg: "Asociacion a tarjeta exitosa"
+    }
+  
+    if (card_id == 13) {
+  
+      res.status(401)
+      res.send("Error al asociar la tarjeta")
+  
+    } else {
+  
+      res.send(JSON.stringify(rta));
+  
+    }
+  
+  });
+
 
 module.exports = router;
